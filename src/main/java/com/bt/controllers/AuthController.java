0e1e9a8e9f05d1a6284c79beb51e110edc573b52
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-06-06 10:28:20 +0200

    Refactor AuthController

--- src/main/java/com/bt/controllers/AuthController.java
+++ src/main/java/com/bt/controllers/AuthController.java
@@ -1,24 +1,12 @@
 package com.bt.controllers;
 
-import com.bt.DAO.AdminDAO;
-import com.bt.DAO.ManagerDAO;
-import com.bt.DAO.UserDAO;
-import com.bt.bean.Admin;
-import com.bt.bean.Manager;
-import com.bt.bean.User;
 import com.bt.utils.AuthenticationUtil;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 
 public class AuthController {
-    AdminDAO adminDAO = new AdminDAO();
-    ManagerDAO managerDAO = new ManagerDAO();
-    UserDAO userDAO = new UserDAO();
-
     AuthenticationUtil authenticationUtil = new AuthenticationUtil();
 
     public void adminController(HttpServletRequest request, HttpServletResponse response) throws Exception {
